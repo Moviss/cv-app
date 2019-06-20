914bfb60c35be97f7f5f9e4dd7e7aa54807a3e64
Marcin Lubowicz <marcin.lubowicz@gmail.com>
2019-06-20 15:30:57 +0200

    Added links and icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import myImage from '../../assets/images/my-image.jpeg';
+// import myImage from '../../assets/images/my-image.jpeg';
 import styles from './Header.module.scss';
+import linkedInLogo from '../../assets/images/linkedin.png';
+import githubLogo from '../../assets/images/GitHub-Mark-32px.png';
+
+const links = [
+  {
+    icon: linkedInLogo,
+    url: 'https://www.linkedin.com/in/marcin-lubowicz/',
+    title: 'LinkedIn'
+  },
+  {
+    icon: githubLogo,
+    url: 'https://github.com/Moviss',
+    title: 'GitHub'
+  }
+  // {
+  //   icon: githubLogo,
+  //   url: 'https://github.com/Moviss',
+  //   title: 'CV Online'
+  // }
+];
 
 const Header = () => {
   return (
@@ -17,6 +37,16 @@ const Header = () => {
           <li>+48 501 619 789</li>
           <li>marcin.lubowicz@gmail.com</li>
         </ul>
+      </div>
+      <div className={styles.links}>
+        {links.map(link => (
+          <div className={styles.link}>
+            <a href={link.url} target="_blank" rel="noopener noreferrer">
+              <img src={link.icon} alt="{link.title}" />
+              <span className={styles.linkTitle}>{link.title}</span>
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/ProfessionalExperience/ProfessionalExperience.js
+++ src/components/ProfessionalExperience/ProfessionalExperience.js
@@ -38,7 +38,6 @@ const jobs = [
       'Design, build, or maintain web sites, using authoring or scripting languages, content creation tools, management tools, and digital media.',
       'Perform or direct web site updates.',
       'Evaluate code to ensure that it is valid, is properly structured, meets industry standards and is compatible with browsers, devices, or operating systems.',
-      'Write, design, or edit web page content, or direct others producing content.',
       'Analyze user needs to determine technical requirements.'
     ]
   }
